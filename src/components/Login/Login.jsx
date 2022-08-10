0a1aeeadc2b6effac2d6259d340925cd76d36d46
WhoSushantRana <110126274+WhoSushantRana@users.noreply.github.com>
2022-08-10 17:06:58 +0530

    styling done (#3)
    Co-authored-by: Sushant Rana <sushantrana@CLPT1170.local>

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-
+import trial_illustration from "../../assets/signn.jpg";
+import google from "../../assets/google.png";
 import { Link, useNavigate } from "react-router-dom";
 import { auth, signInWithGoogle, logInWithEmailAndPassword } from "../firebase.js";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -19,15 +20,29 @@ function Login() {
     if (user) navigate("/dashboard");
   }, [user, loading]);
   return (
-    <div className="login">
-      <div className="login__container">
-        <input
+   
+  
+    <section id="trial">
+    <div className="container">
+        <div className="content">
+          <img
+            src={trial_illustration}
+            alt="illustration"
+            className="img-fluid"
+          />
+          <div className="text_section">
+            <h1 className="trial_title">curious?</h1>
+            <h3 className="trial_description">
+              Sign up for a FREE trial – 1 free test!
+              <br /> Or book a demo – we’d love to show you around!
+            </h3>
+            <input
           type="text"
           className="login__textBox"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder="E-mail Address"
-        />
+        /><br />
         <input
           type="password"
           className="login__textBox"
@@ -35,23 +50,33 @@ function Login() {
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
         />
-        <button
-          className="login__btn"
+            <div className="buttons">
+            <button
+          className="try_button"
           onClick={() => logInWithEmailAndPassword(email, password)}
         >
           Login
         </button>
-        <button className="login__btn login__google" onClick={signInWithGoogle}>
+              
+              <button className="demo_button" onClick={signInWithGoogle}>
+              <img1 src={google}  />
           Login with Google
         </button>
-        <div>
+            </div>
+            <div>
           <Link to="/reset">Forgot Password</Link>
         </div>
         <div>
           Don't have an account? <Link to="/register">Register</Link> now.
         </div>
+          </div>
+        </div>
       </div>
-    </div>
+      
+      </section> 
+    
+    
+    
   );
 }
 export default Login;
